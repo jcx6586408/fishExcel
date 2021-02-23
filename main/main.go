744b6ee67bac4bef12d8d998d96911cb9137bd36
LarryJody <931490577@qq.com>
2021-02-23 16:45:17 +0800

    提交版本

---
+++ main/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	println("吃鱼表格解析工具")
+
+	// f, err := excelize.OpenFile("./table/物品道具.xlsx")
+	// // f, err := excelize.OpenFile("D:/WorkProgram/GameProgram/MonsterHunter/table/物品道具.xlsx")
+	// if err != nil {
+	// 	println(err.Error())
+	// 	return
+	// }
+}
